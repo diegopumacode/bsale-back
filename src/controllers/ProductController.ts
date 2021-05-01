c774009b148cb12a0e86c27dd2f834f4819becfb
diegopumacode <diego.puma@lilab.pe>
2021-05-01 12:32:53 -0500

    adding getProductByName method in ProductController

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -10,6 +10,7 @@ export default class ProductController {
 
   public initializeRoutes() {
     this.router.get(`${this.path}/category/:id`, this.getProductByCategory);
+    this.router.get(`${this.path}/search/:name`, this.getProductsByName);
     
   }
 
@@ -18,6 +19,13 @@ export default class ProductController {
         if(err) throw err;
         return res.json(rows);
     })
+  }
+  
+  public async getProductsByName(req: express.Request, res: express.Response){
+    app.db.query(`select * from product where product.name LIKE "${req.params.name}%"`,(err,rows,fields)=>{
+        if(err) throw err;
+        return res.json(rows);
+    }) 
   }
 
 }
