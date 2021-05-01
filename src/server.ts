edec0e2f73bf4364f2b0f2a8f1ab3fd094f72002
diegopumacode <diego.puma@lilab.pe>
2021-05-01 12:53:03 -0500

    adding deployment 1.0.1

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import App from "./App";
 import CategoryController from "./controllers/CategoryController.";
+import HealthController from "./controllers/HealthController";
 import ProductController from "./controllers/ProductController";
 
-
-const controllers = [new CategoryController(), new ProductController()];
+const controllers = [new CategoryController(), new ProductController(), new HealthController()];
 const app = new App(controllers, 3000);
 app.start();
 
